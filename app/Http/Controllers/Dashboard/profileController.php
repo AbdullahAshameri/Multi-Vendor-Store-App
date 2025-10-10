1df2648726a9e9c0a8ba182d21ea4bbc5d4fac00
Abdullah Alshameri <abdullah.ashameri01@gmail.com>
2025-10-10 23:40:43 +0300

    Add profileController for dashboard

---
+++ app/Http/Controllers/Dashboard/profileController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+use Symfony\Component\Intl\Countries;
+use Symfony\Component\Intl\Languages;
+
+class profileController extends Controller
+{
+    public function edit()
+    {
+        $user = Auth::user();
+        return view('dashboard.profile.edit', [
+            'user'=>$user,
+            'countries'=>Countries::getNames('en'),
+            'locales'=>Languages::getNames('en'),
+        ]);
+    }
+
+
+    public function update(Request $request)
+    {
+        $request->validate([
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'birthday' => ['nullable', 'date', 'before:today'],
+            'gender' => ['in:male,female'],
+            'country' => ['required', 'string', 'size:2'],
+        ]);
+
+        $user = $request->user();
+
+        $user->profile->fill( $request->all() )->save();
+
+        return Redirect()->route('dashboard.profile.edit')
+            ->with('success', 'profile-updated!');
+    }
+
+    // public function destroy(Request $request): RedirectResponse
+    // {
+    //     $request->validateWithBag('userDeletion', [
+    //         'password' => ['required', 'current-password'],
+    //     ]);
+
+    //     $user = $request->user();
+
+    //     Auth::logout();
+
+    //     $user->delete();
+
+    //     $request->session()->invalidate();
+    //     $request->session()->regenerateToken();
+
+    //     return Redirect::to('/');
+    // }
+}
